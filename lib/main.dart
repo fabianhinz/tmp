34cce04ec01f811d2faab59e56e1210a502edbda
fabianhinz <hinzfabian.fh@googlemail.com>
2019-09-05 14:56:55 +0200

    fixedBug

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
-
+// fixed the bug
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
